8ab68459ee3b83313d4d693f4e0ede5aebc3264c
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2023-08-05 09:43:53 -0700

    Initial Commit

---
+++ GameRank/GameRankApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameRankApp.swift
+//  GameRank
+//
+//  Created by Dylan Conklin on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GameRankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
