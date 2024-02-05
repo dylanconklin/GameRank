489747b2b0b9aaea4beea6748e0d419c049fed16
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-02-05 11:34:15 -0800

    Fixed Preview; Downsized Logo and Scores

--- GameRank/TeamLogo.swift
+++ GameRank/TeamLogo.swift
@@ -10,24 +10,29 @@ import SwiftUI
 struct TeamLogo: View {
     var team: Team
     var score: Int?
-    
+
     var body: some View {
         ZStack {
             Circle()
                 .foregroundColor(team.colors[0])
-                .frame(width: 140, height: 140)
+                .frame(width: 105, height: 105)
             Circle()
                 .foregroundColor(team.colors[1])
-                .frame(width: 120, height: 120)
+                .frame(width: 90, height: 90)
             if let score {
-                Image(systemName: "\(score).circle.fill")
-                    .resizable()
-                    .frame(width: 140, height: 140)
+                Circle()
+                    .frame(width: 105, height: 105)
                     .foregroundStyle(team.colors[0])
+                Text("\(score)")
+                    .frame(width: 105, height: 105)
+                    .font(.largeTitle)
+                    .fontDesign(.monospaced)
+                    .fontWeight(.bold)
+                    .foregroundStyle(team.colors[1])
             } else {
                 Image(systemName: "football.fill")
                     .resizable()
-                    .frame(width: 80, height: 80)
+                    .frame(width: 60, height: 60)
                     .imageScale(.large)
                     .foregroundColor(team.colors[0])
             }
@@ -35,8 +40,8 @@ struct TeamLogo: View {
     }
 }
 
-struct TeamLogo_Previews: PreviewProvider {
-    @State static var team: [Team] = [
+#Preview {
+    @State var team: [Team] = [
         Team(abbreviated_name: "ARI", name: ""),
         Team(abbreviated_name: "ATL", name: ""),
         Team(abbreviated_name: "BAL", name: ""),
@@ -48,41 +53,39 @@ struct TeamLogo_Previews: PreviewProvider {
         Team(abbreviated_name: "DAL", name: ""),
         Team(abbreviated_name: "DEN", name: ""),
         Team(abbreviated_name: "DET", name: ""),
-        Team(abbreviated_name: "GB" , name: ""),
+        Team(abbreviated_name: "GB", name: ""),
         Team(abbreviated_name: "HOU", name: ""),
         Team(abbreviated_name: "IND", name: ""),
         Team(abbreviated_name: "JAX", name: ""),
-        Team(abbreviated_name: "KC" , name: ""),
+        Team(abbreviated_name: "KC", name: ""),
         Team(abbreviated_name: "LAC", name: ""),
         Team(abbreviated_name: "LAR", name: ""),
-        Team(abbreviated_name: "LV" , name: ""),
+        Team(abbreviated_name: "LV", name: ""),
         Team(abbreviated_name: "MIA", name: ""),
         Team(abbreviated_name: "MIN", name: ""),
-        Team(abbreviated_name: "NE" , name: ""),
-        Team(abbreviated_name: "NO" , name: ""),
+        Team(abbreviated_name: "NE", name: ""),
+        Team(abbreviated_name: "NO", name: ""),
         Team(abbreviated_name: "NYG", name: ""),
         Team(abbreviated_name: "NYJ", name: ""),
         Team(abbreviated_name: "PHI", name: ""),
         Team(abbreviated_name: "PIT", name: ""),
         Team(abbreviated_name: "SEA", name: ""),
-        Team(abbreviated_name: "SF" , name: ""),
-        Team(abbreviated_name: "TB" , name: ""),
+        Team(abbreviated_name: "SF", name: ""),
+        Team(abbreviated_name: "TB", name: ""),
         Team(abbreviated_name: "TEN", name: ""),
         Team(abbreviated_name: "WAS", name: ""),
-        ]
-    static var previews: some View {
-        VStack {
-            ScrollView {
-                ForEach (team, id: \.self) { t in
-                    HStack {
-                        Spacer()
-                        Text("\(t.abbreviated_name)")
-                        Spacer()
-                        TeamLogo(team: t)
-                        Spacer()
-                        TeamLogo(team: t, score: 50)
-                        Spacer()
-                    }
+    ]
+    return VStack {
+        ScrollView {
+            ForEach(team, id: \.self) { t in
+                HStack {
+                    Spacer()
+                    Text("\(t.abbreviated_name)")
+                    Spacer()
+                    TeamLogo(team: t)
+                    Spacer()
+                    TeamLogo(team: t, score: 50)
+                    Spacer()
                 }
             }
         }
